bc46d3aa9589acc22ebc5b6c1093853f605eb9cd
andressepulvedak <143463860+andressepulvedak@users.noreply.github.com>
2023-09-08 14:44:36 -0600

    Add files via upload

---
+++ Programas de cálculos.py
@@ -0,0 +1,56 @@
+print("hola mundo")
+
+# 1 Haz un programa que lea 2 números e imprima True si el primero es 20 veces el valor del segundo.
+#Si no, imprime False
+'''
+a = int(input())
+b = int(input())
+
+print(20*a == b)
+
+'''
+# 2 Haz un programa que lea tres números e imprima el producto de los tres dividido entre la suma
+#de los tres
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+print((a * b * c) / (a + b + c))
+
+'''
+# 3 Haz un programa que lea 3 números e imprima True si están en orden ascendiente. Si no, imprime
+#False
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a < b < c)
+
+'''
+# 6 Haz un programa que lea 3 números e imprima True si el primero es menor que el segundo y si
+#el segundo no es igual al tercero. Si no, imprime false
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a < b != c)
+
+'''
+# 8 Haz un programa que lea 8 números e imprima el promedio
+''''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+
+print((a + b + c + d + e + f + g + h) / 8)
+
+'''
+
---
+++ holamundo.py
@@ -0,0 +1,49 @@
+print("hola mundo")
+
+'''
+a = int(input())
+b = int(input())
+
+print(20*a == b)
+
+'''
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print((a * b * c) / (a + b + c))
+
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a < b < c)
+
+'''
+
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a < b != c)
+
+'''
+
+''''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+
+print((a + b + c + d + e + f + g + h) / 8)
+
+'''
+
